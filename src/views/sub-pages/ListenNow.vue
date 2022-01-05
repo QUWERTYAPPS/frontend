d71f37ad57e5590796fdbca3997ce2a93b2fe24d
nikos862 <nikokro70@edu.gdansk.pl>
2022-01-06 00:39:46 +0100

    Fix Button list on ListenNow

--- src/views/sub-pages/ListenNow.vue
+++ src/views/sub-pages/ListenNow.vue
@@ -3,17 +3,19 @@
       <div>
         <h1>Listen Now</h1><br/>
 
-        <div class="scroll-x no-wrap width-100prc display-inline-block">
-          <q-card class="padding-16px margin-16px">
-            <q-btn color="grey-9" :style="{backgroundImage: '@/assets/sample/' + song.cover}" v-for="song in songs" :key="song.id" class="display-flex song-button margin-4px" style="width: 142px">
-              <img :src="require('@/assets/sample/' + song.cover)" class="rounded-borders float-left" width="128" height="128">
-              <div style="width: 100%"/>
-              <div class="margin-4px">
-                <p class="song-title-tile">{{song.title}}</p>
-                <p class="song-author-tile">{{song.author}}</p>
-                <p class="song-album-tile">{{song.album}}</p>
-              </div>
-            </q-btn>
+        <div class="width-100prc display-inline-block">
+          <q-card class=" margin-16px">
+            <div class="padding-16px scroll-x no-wrap">
+              <q-btn color="grey-9" :style="{backgroundImage: '@/assets/sample/' + song.cover}" v-for="song in songs" :key="song.id" class="display-flex song-button margin-4px" style="width: 142px">
+                <img :src="require('@/assets/sample/' + song.cover)" class="rounded-borders float-left" width="128" height="128">
+                <div style="width: 100%"/>
+                <div class="margin-4px">
+                  <p class="song-title-tile">{{song.title}}</p>
+                  <p class="song-author-tile">{{song.author}}</p>
+                  <p class="song-album-tile">{{song.album}}</p>
+                </div>
+              </q-btn>
+            </div>
           </q-card>
         </div>
       </div>
