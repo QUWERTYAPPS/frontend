ae1e2fe7d713a186491ffe5244c23eed6a9d94ef
themixhelp <themixhelp@outlook.com>
2022-01-09 02:15:04 +0100

    note from the editor + setting the default language in index.html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,5 @@
+<!-- i'm glad it works, good luck to the next person who develops this component (I was going to rebuild it but I'm afraid to touch it 🧐) -->
+
 <template>
 	<div>
 		<q-card class="padding-16">
@@ -7,19 +9,23 @@
 					<q-btn @click="playing = !playing" color="primary" :icon="playing ? 'pause' : 'play_arrow'" />
 					<q-btn color="primary" icon="skip_next" />
 				</q-btn-group>
+
 				<div class="absolute-x-center bottom-border-radius width-300 padding-16 bg-grey-9">
-					<img class="rounded-borders" src="@/assets/samples/nevermind.jpg" width="30" />
+					<img class="rounded-borders" src="@/assets/samples/nevermind.jpg" width="30" alt="" />
 					<div class="absolute-x-center margin-top-16">
 						<div>{{ title }} || {{ author }}</div>
 						<div class="align-center color-grey-6">{{ album }}</div>
 					</div>
 				</div>
+
 				<q-btn-group class="absolute-top-right margin-16">
 					<q-btn @click="loop = !loop" :color="loop ? 'primary' : 'default'" icon="loop" />
 					<q-btn @click="shuffle = !shuffle" :color="shuffle ? 'primary' : 'default'" icon="shuffle" />
 				</q-btn-group>
 			</div>
+
 			<br />
+
 			<q-tabs v-model="active">
 				<q-tab name="listen-now" label="Listen now" />
 				<q-tab name="explore" label="Explore" />
